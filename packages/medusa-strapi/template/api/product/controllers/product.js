49877b61313e2b3b1a85f64f247ead09571a0847
Pawan Sharma <pawan.s1998@gmail.com>
2021-11-03 23:11:50 +0530

    response.js fixes

--- packages/medusa-strapi/template/api/product/controllers/product.js
+++ packages/medusa-strapi/template/api/product/controllers/product.js
@@ -1,7 +1,5 @@
-'use strict';
+"use strict"
 
-const {badRequest} = require("../../../utils/response");
-const {serverError, notFound, success} = require("../../../utils/response");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -10,82 +8,91 @@ const {serverError, notFound, success} = require("../../../utils/response");
 module.exports = {
   async findOne(ctx) {
     try {
-      const { productId } = ctx.params;
-      const product = await strapi.query('product', '').findOne({ product_id: productId });
+      const { productId } = ctx.params
+      const product = await strapi
+        .query("product", "")
+        .findOne({ product_id: productId })
       if (product && product.id) {
-        return success(ctx, { product });
+        return strapi.config.functions.response.success(ctx, { product })
       }
-      return notFound(ctx);
-
+      return strapi.config.functions.response.notFound(ctx)
     } catch (e) {
-      return serverError(ctx, e);
+      return strapi.config.functions.response.serverError(ctx, e)
     }
-
   },
   async create(ctx) {
     try {
-      const productBody = ctx.request.body;
+      const productBody = ctx.request.body
 
-      const create = await strapi.services['product'].createWithRelations(productBody);
+      const create = await strapi.services.product.createWithRelations(
+        productBody
+      )
       if (create) {
-        return success(ctx, { id: create });
+        return strapi.config.functions.response.success(ctx, { id: create })
       }
-      return badRequest(ctx);
-
+      return strapi.config.functions.response.badRequest(ctx)
     } catch (e) {
-      return serverError(ctx, e);
+      return strapi.config.functions.response.serverError(ctx, e)
     }
   },
   async update(ctx) {
     try {
-      const { medusaId } = ctx.params;
-      const productBody = ctx.request.body;
+      const { medusaId } = ctx.params
+      const productBody = ctx.request.body
 
-      productBody.product_length = productBody.length;
-      delete productBody.length;
+      productBody.product_length = productBody.length
+      delete productBody.length
 
-      const found = await strapi.query('product', '').findOne({
-        medusa_id: medusaId
-      });
+      const found = await strapi.query("product", "").findOne({
+        medusa_id: medusaId,
+      })
 
       if (found) {
-        const update = await strapi.services['product'].updateWithRelations(productBody);
+        const update = await strapi.services["product"].updateWithRelations(
+          productBody
+        )
         if (update) {
-          return success(ctx, { id: update });
+          return strapi.config.functions.response.success(ctx, { id: update })
         } else {
-          return serverError(ctx, 'ERROR');
+          return strapi.config.functions.response.serverError(ctx, "ERROR")
         }
       }
 
-      const create = await strapi.services['product'].createWithRelations(productBody);
+      const create = await strapi.services["product"].createWithRelations(
+        productBody
+      )
       if (create) {
-        return success(ctx, { id: create });
+        return strapi.config.functions.response.success(ctx, { id: create })
       }
 
-      return notFound(ctx);
+      return strapi.config.functions.response.notFound(ctx)
     } catch (e) {
-      return serverError(ctx, e);
+      return strapi.config.functions.response.serverError(ctx, e)
     }
   },
   async delete(ctx) {
     try {
-      const { medusaId } = ctx.params;
-      const product = await strapi.query('product', '').findOne({ medusa_id: medusaId });
+      const { medusaId } = ctx.params
+      const product = await strapi
+        .query("product", "")
+        .findOne({ medusa_id: medusaId })
       if (product) {
         if (product.product_variants && product.product_variants.length) {
-          await strapi.query('product-variant', '').delete({ product: product.id });
+          await strapi
+            .query("product-variant", "")
+            .delete({ product: product.id })
         }
-        await strapi.query('product', '').delete({
-          medusa_id: medusaId
-        });
-        return success(ctx, {
-          id: product.id
-        });
+        await strapi.query("product", "").delete({
+          medusa_id: medusaId,
+        })
+        return strapi.config.functions.response.success(ctx, {
+          id: product.id,
+        })
       }
-      return notFound(ctx)
+      return strapi.config.functions.response.notFound(ctx)
     } catch (e) {
-      console.log('Error occurred while trying to delete product variant');
-      return serverError(ctx, e);
+      console.log("Error occurred while trying to delete product variant")
+      return strapi.config.functions.response.serverError(ctx, e)
     }
-  }
-};
+  },
+}
--- packages/medusa-strapi/template/api/region/controllers/region.js
+++ packages/medusa-strapi/template/api/region/controllers/region.js
@@ -1,5 +1,4 @@
-'use strict';
-const {notFound, serverError, success, badRequest} = require("../../../utils/response");
+"use strict"
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -9,57 +8,67 @@ const {notFound, serverError, success, badRequest} = require("../../../utils/res
 module.exports = {
   async findOne(ctx) {
     try {
-      const { medusaId } = ctx.params;
-      const region = await strapi.query('region', '').findOne({ region_id: medusaId });
+      const { medusaId } = ctx.params
+      const region = await strapi
+        .query("region", "")
+        .findOne({ region_id: medusaId })
       if (region && region.id) {
-        return success(ctx, { region });
+        return strapi.config.functions.response.success(ctx, { region })
       }
-      return notFound(ctx);
+      return strapi.config.functions.response.notFound(ctx)
     } catch (e) {
-      return serverError(ctx);
+      return strapi.config.functions.response.serverError(ctx)
     }
-
   },
   async create(ctx) {
     try {
-      const regionBody = ctx.request.body;
-      Object.keys(regionBody).forEach(key => regionBody[key] === undefined && delete regionBody[key]);
+      const regionBody = ctx.request.body
+      Object.keys(regionBody).forEach(
+        (key) => regionBody[key] === undefined && delete regionBody[key]
+      )
 
-      const create = await strapi.services.region.createWithRelations(regionBody);
+      const create = await strapi.services.region.createWithRelations(
+        regionBody
+      )
       if (create) {
-        return success(ctx, { id: create });
+        return strapi.config.functions.response.success(ctx, { id: create })
       }
-      return badRequest(ctx);
-
+      return strapi.config.functions.response.badRequest(ctx)
     } catch (e) {
-      return serverError(ctx, e);
+      return strapi.config.functions.response.serverError(ctx, e)
     }
   },
   async update(ctx) {
     try {
-      const { medusaId } = ctx.params;
-      const regionBody = ctx.request.body;
-      Object.keys(regionBody).forEach(key => regionBody[key] === undefined && delete regionBody[key]);
+      const { medusaId } = ctx.params
+      const regionBody = ctx.request.body
+      Object.keys(regionBody).forEach(
+        (key) => regionBody[key] === undefined && delete regionBody[key]
+      )
 
-      const found = await strapi.query('region', '').findOne({
-        medusa_id: medusaId
-      });
+      const found = await strapi.query("region", "").findOne({
+        medusa_id: medusaId,
+      })
 
       if (found) {
-        const update = await strapi.services.region.updateWithRelations(regionBody);
+        const update = await strapi.services.region.updateWithRelations(
+          regionBody
+        )
         if (update) {
-          return success(ctx, { id: update });
+          return strapi.config.functions.response.success(ctx, { id: update })
         }
       }
 
-      const create = await strapi.services.region.createWithRelations(regionBody);
+      const create = await strapi.services.region.createWithRelations(
+        regionBody
+      )
       if (create) {
-        return success(ctx, { id: create });
+        return strapi.config.functions.response.success(ctx, { id: create })
       }
 
-      return notFound(ctx);
+      return strapi.config.functions.response.notFound(ctx)
     } catch (e) {
-      return serverError(ctx, e);
+      return strapi.config.functions.response.serverError(ctx, e)
     }
-  }
-};
+  },
+}
