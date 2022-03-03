f04c5bde02713ff53f02414fb9b26763efbe70e0
Kyle Diggs <kyle@mintgolddust.io>
2022-03-03 14:30:49 -0800

    converting product-type

--- packages/medusa-strapi/template/src/api/product-type/controllers/product-type.js
+++ packages/medusa-strapi/template/src/api/product-type/controllers/product-type.js
@@ -1,8 +1,9 @@
 'use strict';
 
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
+ *  product-type controller
  */
 
-module.exports = {};
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::product-type.product-type');
