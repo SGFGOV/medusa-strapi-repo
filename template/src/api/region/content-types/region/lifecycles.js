9b65154573658abbcbe8d08fd2897e9823cb984c
Kyle Diggs <kyle@mintgolddust.io>
2022-03-03 17:06:44 -0800

    updating lifecycle structure

--- template/src/api/region/content-types/region/lifecycles.js
+++ template/src/api/region/content-types/region/lifecycles.js
@@ -7,16 +7,14 @@
 const axios = require('axios');
 
 module.exports = {
-  lifecycles: {
-    async afterUpdate(result, params, data) {
-      await axios.post('http://localhost:9000/hooks/update-medusa', {
-        type: 'region',
-        data
-      }, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-    }
+  async afterUpdate(result, params, data) {
+    await axios.post('http://localhost:9000/hooks/update-medusa', {
+      type: 'region',
+      data
+    }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   }
 };
