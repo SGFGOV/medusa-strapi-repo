cd32203f2f601cd96c5c4a7d1e22b9fa2d41615f
SGF <SGF@sourcegoodfood.com>
2022-12-16 05:57:03 +0000

    fix: updated to fix strapi sync

--- config/middlewares.js
+++ config/middlewares.js
@@ -31,7 +31,29 @@ module.exports = ({ env }) => ({
 },
 [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:','http:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // cloudinary images
+            'lh3.googleusercontent.com', // google avatars
+            'platform-lookaside.fbsbx.com', // facebook avatars
+            'dl.airtable.com', // strapi marketplace
+            `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`
+          ],
+          'media-src': ["'self'", 'data:', 'blob:', `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   {
     name: 'strapi::poweredBy',
