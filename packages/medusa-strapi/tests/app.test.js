57846243ea1c65c4027a201f60c7a0542104d1ab
SGF <SGF@sourcegoodfood.com>
2022-12-26 06:28:45 +0000

    feat: test environment

---
+++ packages/medusa-strapi/tests/app.test.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const { setupStrapi, cleanupStrapi } = require("./helpers/strapi");
+jest.setTimeout(3e6)
+beforeAll(async () => {
+  await setupStrapi();
+});
+
+afterAll(async () => {
+  await cleanupStrapi();
+});
+
+it("strapi is defined", () => {
+  expect(strapi).toBeDefined();
+},10000);
