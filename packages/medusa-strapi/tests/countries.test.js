089dacfd7e8f7f47582636dfec496c7983bafb69
SGF <SGF@sourcegoodfood.com>
2022-12-26 06:35:29 +0000

    feat: test environment with basic test

--- packages/medusa-strapi/tests/countries.test.js
+++ packages/medusa-strapi/tests/countries.test.js
@@ -1,3 +1,4 @@
+const request = require('supertest');
 const { setupStrapi, cleanupStrapi } = require("./helpers/strapi");
 jest.setTimeout(3e6)
 beforeAll(async () => {
@@ -11,3 +12,9 @@ afterAll(async () => {
 it("strapi is defined", () => {
   expect(strapi).toBeDefined();
 },10000);
+
+it("should return 200", async () => {
+    await request(strapi.server.httpServer)
+      .get("/api/countries")
+      .expect(200) // Expect response http code 200
+  });
