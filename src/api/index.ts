e6a36c88e51202ec2f50126d2839b91087e66ebb
SGF <SGF@sourcegoodfood.com>
2023-02-04 06:08:08 +0000

    feat: sso routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import hooksRouter from "./routes/hooks";
 import contentRouter from "./routes/content";
 import { ConfigModule } from "@medusajs/medusa/dist/types/global";
+import adminRouter from "./routes/strapi-admin";
 
 export default (app, options, config: ConfigModule) => {
     //  app.use("/strapi", strapiRouter);
@@ -10,6 +11,7 @@ export default (app, options, config: ConfigModule) => {
 
     hooksRouter(strapiRouter, options);
     contentRouter(strapiRouter, options, config);
+    adminRouter(strapiRouter, options, config);
 
     return strapiRouter;
 };
