3c8062fb459ad3d7e2106bbc6bca80375fa54452
SGF <SGF@sourcegoodfood.com>
2023-03-03 03:18:40 +0000

    fix: dependencies

--- packages/medusa-strapi/config/admin.js
+++ packages/medusa-strapi/config/admin.js
@@ -1,22 +1,20 @@
-//const sgftechSaasFormStrategy = require("@sgftech/passport-saasform");
+// const sgftechSaasFormStrategy = require("@sgftech/passport-saasform");
 
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env("ADMIN_JWT_SECRET"),
-    events: {
-      onConnectionSuccess(e) {},
-      onConnectionError(e) {},
-      // ...
-      onSSOAutoRegistration(e) {
-        const { user, provider } = e;
+	auth: {
+		secret: env('ADMIN_JWT_SECRET'),
+		events: {
+			onConnectionSuccess(e) {},
+			onConnectionError(e) {},
+			// ...
+			onSSOAutoRegistration(e) {
+				const { user, provider } = e;
 
-        console.log(
-          `A new user (${user.id}) has been automatically registered using ${provider}`
-        );
-      },
-    },
-    providers: [
-    /*  {
+				console.log(`A new user (${user.id}) has been automatically registered using ${provider}`);
+			},
+		},
+		providers: [
+			/*  {
         uid: "saasform",
         displayName: "SGF-Login",
         icon: "",
@@ -40,13 +38,10 @@ module.exports = ({ env }) => ({
             }
           ),
       },*/
-    ],
-  },
-  apiToken: {
-    salt: env("API_TOKEN_SALT"),
-  },
-  rateLimit:
-    process.env.NODE_ENV == "test"
-      ? { enabled: false }
-      : { interval: 60000, max: 100000 },
+		],
+	},
+	apiToken: {
+		salt: env('API_TOKEN_SALT'),
+	},
+	rateLimit: process.env.NODE_ENV == 'test' ? { enabled: false } : { interval: 60000, max: 100000 },
 });
