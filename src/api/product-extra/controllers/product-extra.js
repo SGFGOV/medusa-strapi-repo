63a59f4261306b7f21b4490a1c1cd14173bb3d5e
SGF <SGF@sourcegoodfood.com>
2023-03-23 00:49:20 +0000

    fix: workflow

---
+++ src/api/product-extra/controllers/product-extra.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ *  product-metafield controller
+ */
+
+const createMedusaDefaultController =
+  require("../../../utils/utils").createMedusaDefaultController;
+const uid = "api::product-extra.product-extra";
+module.exports = createMedusaDefaultController(uid);
